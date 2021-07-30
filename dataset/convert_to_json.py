7ac31d3b42ab8310989c3df7470dac37894ae9a8
Alex <alexandru.pavel.94@gmail.com>
2021-07-30 18:48:21 +0200

    convert graph data to one json file (like d3 docs)

---
+++ dataset/convert_to_json.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import csv
+import json
+
+nodes_json = []
+edges_json = []
+graph_json = {}
+
+with open('graph_nodes.csv') as nodes:
+	csv_reader = csv.reader(nodes, delimiter=',')
+	next(csv_reader)
+	fields_list = ["id", "label", "gender", "gender description", "chapter", "page"]
+
+	for row in csv_reader:
+		obj = {}
+		for i, field in enumerate(fields_list):
+			obj[field] = row[i]
+		nodes_json.append(obj)
+
+with open('graph_edges.csv') as edges:
+	csv_reader = csv.reader(edges, delimiter=',')
+	next(csv_reader)
+	fields_list = ["source", "target", "action", "action description", "chapter", "page"]
+
+	for row in csv_reader:
+		obj = {}
+		for i, field in enumerate(fields_list):
+			obj[field] = row[i]
+		edges_json.append(obj)
+
+graph_json['nodes'] = nodes_json
+graph_json['links'] = edges_json
+
+with open('graph.json', 'w') as json_file:
+	json_file.write(json.dumps(graph_json, indent=4))
